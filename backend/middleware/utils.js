1fd5d72f19907badb8aa09a3d93a57e9204c1b36
nidhi <nidhi7485@gmail.com>
2023-05-21 08:11:56 +0530

    change in v-token-fun

--- backend/middleware/utils.js
+++ backend/middleware/utils.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization
+  const token = req.cookies.token
 
   if (!token) {
     return res.status(401).json({ message: 'No token provided' })
